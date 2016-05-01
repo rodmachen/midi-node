5c75818ac7dc558280be119326ab4bff570a9456
Rod Machen <mail@rodmachen.com>
2016-05-01 08:42:29 -0500

    Update with midi and midi-duplex

--- 2016/04/30/app.js
+++ 2016/04/30/app.js
@@ -1,13 +1,19 @@
-// Launchpad MK2
-
+// port name: Launchpad MK2
+var midi = require('midi')
 var MidiStream = require('midi-stream')
- 
+
+var input = new midi.input();
+var output = new midi.output();
+output.openPort(0);
+
 var duplex = MidiStream('Launchpad MK2', 0)
 
 // write buttons pressed to console 
 duplex.on('data', function(data){
   console.log(data)
 })
+
+console.log(output.getPortCount())
 
 // squares:
 // top: [ 144, 81-88, 127 ]
@@ -17,18 +23,42 @@ duplex.on('data', function(data){
 // top: [ 176, 104-111, 127 ]
 // right: [ 144, 19-89 (by 10s), 127 ]
 
-// duplex.write([ 144, 21, 0 ])
+// duplex.write([ 144, 11, 32 ]);
+// duplex.write([ 144, 12, 32 ]);
+// duplex.write([ 144, 13, 32 ]);
+// duplex.write([ 144, 21, 32 ]);
+// duplex.write([ 144, 21, 32 ]);
+// duplex.write([ 144, 22, 12 ]);
+// duplex.write([ 144, 31, 32 ]);
+// duplex.write([ 144, 32, 32 ]);
+output.sendMessage([ 144, 33, 60 ]);
+
+var squareList = [11, 21, 31, 32, 33, 23, 13, 12];
+
+// for (var j = 0; j < 22; j++) {
+// 	setTimeout(() => makeShape(squareList, j), 100);
+// 	// makeShape(squareList, j)
+// }
+
+function makeShape(list, color) {
+	for (var i = 0; i < list.length; i++) {
+		play(list[i], color, 100 * i);
+		// duplex.write([144, list[i], color]);
+	}
+}
+
+makeShape(squareList, 0);
 
 function play(note, velocity, delay) {
 	setTimeout(() => duplex.write([144, note, velocity]), delay);
 }
 
-for (var velocity = 0; velocity < 128; velocity++) {
-	for (var key = 11; key < 90 ; key++) {
-		play(key, 0, 0)
-		// play(key, velocity, (velocity * 80) + (key * 10))
-	}
-}
+// for (var velocity = 0; velocity < 128; velocity++) {
+// 	for (var key = 11; key < 90 ; key++) {
+// 		play(key, 0, 0)
+// 		// play(key, velocity, (velocity * 80) + (key * 10))
+// 	}
+// }
 // var faceCoords = [
 //   [1,1], [2,1], [5,1], [6,1],
 //   [1,2], [2,2], [5,2], [6,2],
@@ -49,3 +79,10 @@ for (var velocity = 0; velocity < 128; velocity++) {
 //   duplex.end()
 // }, 5000)
 
+input.on('message', function(deltaTime, message) {
+  // The message is an array of numbers corresponding to the MIDI bytes: 
+  //   [status, data1, data2] 
+  // https://www.cs.cf.ac.uk/Dave/Multimedia/node158.html has some helpful 
+  // information interpreting the messages. 
+  console.log('m:' + message + ' d:' + deltaTime);
+});
